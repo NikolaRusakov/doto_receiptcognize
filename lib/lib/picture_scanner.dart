25aaf60df850b435009212422d058f8fb014ca1b
Nikola Rusakov <nikola.rusakov@rusakov.cz>
2020-01-12 13:21:04 +0100

    test(detection): include ml vision

--- lib/lib/picture_scanner.dart
+++ lib/lib/picture_scanner.dart
@@ -120,20 +120,20 @@ class _PictureScannerState extends State<PictureScanner> {
       default:
         return;
     }
-    VisionText _currentText;
-    _currentText = results;
-    print(_currentText.blocks.asMap());
-    for (var text in _currentText.blocks) {
-      print("text : ${text.text}");
-    }
+//    VisionText _currentText;
+//    _currentText = results;
+//    print(_currentText.blocks.asMap());
+//    for (var text in _currentText.blocks) {
+//      print("text : ${text.text}");
+//    }
     setState(() {
       _scanResults = results;
-      final MediaQueryData screenSize = MediaQuery.of(context);
-      final double scaleX = screenSize.size.width / _imageSize.width;
-      final double scaleY = screenSize.size.height / _imageSize.height;
-      List<double> screen = [scaleX, scaleY];
-      _detectedTextBloc
-          .dispatch(SaveDetectedText(text: _currentText, screen: screen));
+//      final MediaQueryData screenSize = MediaQuery.of(context);
+//      final double scaleX = screenSize.size.width / _imageSize.width;
+//      final double scaleY = screenSize.size.height / _imageSize.height;
+//      List<double> screen = [scaleX, scaleY];
+//      _detectedTextBloc
+//          .dispatch(SaveDetectedText(text: _currentText, screen: screen));
     });
   }
 
@@ -431,15 +431,6 @@ class _MultiTapRecognizeState extends State<MultiTapRecognize> {
     );
   }
 }
-
-class DrawingPoints {
-  Paint paint;
-  Offset points;
-
-  DrawingPoints({this.points, this.paint});
-}
-
-typedef onPaintRectangleCallBack = void Function(List<Offset> points);
 
 class ConstraintPainter extends CustomPainter {
   ConstraintPainter(
